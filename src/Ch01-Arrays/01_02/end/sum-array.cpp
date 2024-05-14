cb09a5d10220f41d3f784d5cb4e719d80f0d06a3
Hiran Kodithuwakku <93377230+HiranKodi@users.noreply.github.com>
2024-05-14 23:34:26 +0000

    Sum Array

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    //...TODO: implement
+    int sum = 0;
+    if (arr)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            sum += arr[i];
+        }
+    }
+
+    return sum;
 }
 
 int main()
 {
-    const int ints[]{1, -7, 17};
+    const int ints[]{};
     cout << "The sum of all elements in the array is " << calculateSum(ints, 3) << endl;
 }
