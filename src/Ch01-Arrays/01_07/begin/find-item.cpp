e482c2675e12683e4182eb58fcf4df9255c282a0
Hiran Kodithuwakku <93377230+HiranKodi@users.noreply.github.com>
2024-05-15 01:01:24 +0000

    Find Item

--- src/Ch01-Arrays/01_07/begin/find-item.cpp
+++ src/Ch01-Arrays/01_07/begin/find-item.cpp
@@ -5,7 +5,16 @@ using namespace std;
 template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
-    // TODO: implement
+    if (size == 0)
+        return -1;
+    else
+    {
+        for (size_t i = 0; i < size; i++)
+        {   if(arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
 }
 
 int main()
